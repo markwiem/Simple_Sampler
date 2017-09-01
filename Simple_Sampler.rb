4909e809d9b1160c52de6a1791ed8171765f4e9d
markwiem <litterbit@gmail.com>
2017-09-01 01:10:02 -0700

    prelim support for banks of samples

--- Simple_Sampler.rb
+++ Simple_Sampler.rb
@@ -2,18 +2,23 @@
 # Mark Wiem 2017/08/24
 ######################################
 #16 Pad Sampler built using SonicPi 3.0
+#loads a sample bank equal to the number of sample pads used,
+#should the number of samples in the bank be less than the number of pads,
+#the index will roll over to the beginning of the sample list again
 
-#load your local sample of choice
-juicy_sound = '/Users/markwiem/Documents/CODE/Sonic_Pi/Samples/TestSamples/BeNice.wav'
+#load your local sample bank
+juicy_sound = '/Users/markwiem/Documents/CODE/Sonic_Pi/Samples/TestSamples/'
+
+#print sample_groups(juicy_sound)
 
 #select the start positions, scaled between 0-1, for the playback points, as many as you'd like
-start_location = [0.25, 0.45, 0.9, 0.0,
+start_location = [0.25, 0.45, 0.9, 0.78,
                   0.0, 0.0, 0.0, 0.0,
                   0.0, 0.0, 0.0, 0.0,
                   0.0, 0.0, 0.0, 0.0]
 
 #desired play_length of sample play in seconds
-play_length = [0.0125, 0.01, 0.01, 0.0,
+play_length = [0.0125, 0.01, 0.01, 0.01,
                0.0, 0.0, 0.0, 0.0,
                0.0, 0.0, 0.0, 0.0,
                0.0, 0.0, 0.0, 0.0,]
@@ -32,7 +37,7 @@ live_loop :midi_piano do
   samp, trigger = sync "/midi/microkontrol_port_b/2/1/control_change"
   for i in range(0, number_of_pads+1);
     if samp == pad_channel[i] and (trigger == 0 or trigger == 127);
-      sample juicy_sound, start: start_location[i], finish: (start_location[i] + play_length[i]),
+      sample juicy_sound, i.round, start: start_location[i], finish: (start_location[i] + play_length[i]),
         attack: 0.1, release: 0.1, rate: 1
     end
   end
